4b1c0c2c1e0122400be2b70ffa15251e32685911
schedrov <rocolusso@icloud.com>
2022-04-06 22:24:28 +0300

    init complete

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ let step = +prompt('Введите степень');
 let result = null;
 
 function mathFunc (num,step= 1){
-
     if (isNaN(num) || isNaN(step)) {
         return console.log('some error')
     } else {
         result = Math.pow(num,step);
-        return console.log(result);
+        return result;
     }
 
 }
 
 mathFunc(num,step);
+
+if (result !== null){
+    alert(result);
+}
+
+
+
+
+
+
 
